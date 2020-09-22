dc28d49da12bbd1a6c86d2e411cdc18a38310ad9
Chen Weiguang <chen.weiguang@gmail.com>
2020-09-22 11:15:45 +0800

    Switch to native python3 for RHEL8.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,78 +2,22 @@
 set -euo pipefail
 
 # Global variables, can override if preferred
-PYTHON_VERSION=${PYTHON_VERSION:-3.8.5}
 ANSIBLE_VERSION=${ANSIBLE_VERSION:-2.7.18}
 
 # This script installs purely to user, but does require certain prequisites that
 # need root access
-
-if command -v pyenv > /dev/null; then
-    echo "pyenv already installed, nothing to do..."
-    exit 0
-fi
 
 if ! command -v sudo > /dev/null; then
     echo "Need sudo to check and install prerequisites."
     exit 1
 fi
 
-# Set up prerequisites
-echo "Checking and installing prerequisites for pyenv..."
-sudo dnf upgrade
-sudo dnf install -y curl ca-certificates git
-
-# Installs to PYENV_ROOT="${HOME}/.pyenv" by default
-curl https://pyenv.run | bash
-
-# Set to bashrc for persistent PATH effect
-cat <<"EOT" >> "${HOME}/.bashrc"
-
-export PATH="${HOME}/.pyenv/bin:${PATH}"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-EOT
-
-# Need to temporarily disable u flag
-set +u
-
-# And also allow this session to have the effect
-# (source doesn't work in subshell)
-export PATH="${HOME}/.pyenv/bin:${PATH}"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-pyenv --version
-set -u
-
-# Set the global version
-echo "Checking and installing prerequisites for python build..."
-sudo dnf install -y \
-    "@Development Tools" \
-    wget \
-    llvm \
-    openssl-devel \
-    tk-devel \
-    libffi-devel \
-    readline-devel \
-    ncurses-devel \
-    sqlite-devel \
-    xz-devel \
-    zlib-devel \
-    bzip2-devel
-
-# Get rid of the installed python2 symbolic link due to python-openssl to prevent confusion
-# echo "Get rid of python2 from PATH before install python via pyenv..."
-# sudo rm -f /usr/bin/python2
-
-pyenv install "${PYTHON_VERSION}"
-pyenv global "${PYTHON_VERSION}"
-
-# Test the install python works
-python --version
+# Ensure python3 is installed
+# install_pyenv_python
+sudo dnf install -y python3 python3-pip
 
 echo "Install ansible via pip..."
-pip install "ansible==${ANSIBLE_VERSION}"
+python3 -m pip install "ansible==${ANSIBLE_VERSION}"
 
 # Test the install python works
 ansible --version
@@ -82,4 +26,4 @@ echo "Install python3-dnf to allow Ansible to run dnf..."
 sudo dnf install -y python3-dnf
 
 echo "Install setuptools to allow Ansible to run pip..."
-pip install --no-cache-dir setuptools==50
+python3 -m pip install --no-cache-dir setuptools==50
