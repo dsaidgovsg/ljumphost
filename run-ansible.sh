8dfb0220c14a09990d95bbd1a9fa86ec2be4b956
Chen Weiguang <chen.weiguang@gmail.com>
2020-09-22 10:43:30 +0800

    Set ansible interpreter correctly.

--- run-ansible.sh
+++ run-ansible.sh
@@ -3,4 +3,7 @@
 # Get script directory: https://stackoverflow.com/a/246128
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ANSIBLE_PYTHON_INTERPRETER="$(command -v python3)" ansible-playbook --connection=local -i "${DIR}/ansible_hosts"  "${DIR}/ansible/site.yml"
+ansible-playbook \
+    --connection=local -i "${DIR}/ansible_hosts" \
+    -e ansible_python_interpreter="$(command -v python3)" \
+    "${DIR}/ansible/site.yml"
