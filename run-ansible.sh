71802bcda954a1db6a53387e22a8e5fbe8249cee
Chen Weiguang <chen.weiguang@gmail.com>
2020-10-07 17:10:16 +0800

    Use inline command to do localhost connection.

--- run-ansible.sh
+++ run-ansible.sh
@@ -6,7 +6,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ALLOW_CONVENIENCE="${ALLOW_CONVENIENCE:-true}"
 
 ansible-playbook \
-    --connection=local -i "${DIR}/ansible_hosts" \
-    -e ansible_python_interpreter="$(command -v python3)" \
+    -i "localhost," \
+    -c "local" \
+    -e "ansible_python_interpreter=$(command -v python3)" \
     -e allow_convenience="${ALLOW_CONVENIENCE}" \
     "${DIR}/ansible/site.yml"
