3e7f9d2eb1248874bd2687ba9475c249860dac1b
kr <kangren.chia@gmail.com>
2021-03-29 15:49:47 +0800

    chore(utils): add tmux, emacs, ag (#4)
    * chore(tmux): add tmux, emacs and misc packages

--- run-ansible.sh
+++ run-ansible.sh
@@ -4,10 +4,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 ALLOW_CONVENIENCE="${ALLOW_CONVENIENCE:-true}"
+ALLOW_EMACS="${ALLOW_EMACS:-false}"
 
 ansible-playbook \
     -i "localhost," \
     -c "local" \
     -e "ansible_python_interpreter=$(command -v python3)" \
     -e allow_convenience="${ALLOW_CONVENIENCE}" \
+    -e allow_emacs="${ALLOW_EMACS}" \
     "${DIR}/ansible/site.yml"
